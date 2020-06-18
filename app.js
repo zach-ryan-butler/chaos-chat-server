31715e943d1e6b109fe02ff6bf1ad05e1af3706d
Zach Butler <zach.standley93@gmail.com>
2020-06-18 12:31:36 -0700

    made server side socket connection functionality

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require("http");
 const socketIo = require("socket.io");
 
 const port = process.env.PORT || 4001;
-const index = require("");
+const index = require("./routes/index");
 
 const app = express();
 app.use(index);
@@ -12,4 +12,24 @@ const server = http.createServer(app);
 
 const io = socketIo(server);
 
-const getApiAndEmit = "TODO";
+let interval;
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+  if(interval) {
+    clearInterval(interval);
+  }
+
+  interval = setInterval(() => getApiAndEmit(socket), 1000);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+    clearInterval(interval);
+  });
+});
+
+const getApiAndEmit = socket => {
+  const response = new Date();
+  socket.emit("FromAPI", response);
+};
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
